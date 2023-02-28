36f7db6a8146d14f841c818e8762040dedaac33d
Ronit Banerjee <ronit.banerjee@nike.com>
2023-02-28 17:45:17 +0530

    Changes

--- src/main/java/com/example/postservice/PostServiceApplication.java
+++ src/main/java/com/example/postservice/PostServiceApplication.java
@@ -3,13 +3,14 @@ package com.example.postservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
 import org.springframework.data.mongodb.repository.config.EnableReactiveMongoRepositories;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 
 @SpringBootApplication
+@EnableAutoConfiguration(exclude={MongoAutoConfiguration.class})
 @EnableGlobalMethodSecurity(prePostEnabled = true, securedEnabled = true)
 @EnableReactiveMongoRepositories
-@EnableAutoConfiguration
 public class PostServiceApplication {
 
 	public static void main(String[] args) {
