4f6dc7641f4f74425061155101a94eb245f346f0
Ronit Banerjee <ronit.banerjee@nike.com>
2023-02-28 17:17:50 +0530

    Changes

--- src/main/java/com/example/postservice/PostServiceApplication.java
+++ src/main/java/com/example/postservice/PostServiceApplication.java
@@ -3,7 +3,6 @@ package com.example.postservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.data.mongodb.repository.config.EnableReactiveMongoRepositories;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 
@@ -11,7 +10,6 @@ import org.springframework.security.config.annotation.method.configuration.Enabl
 @EnableGlobalMethodSecurity(prePostEnabled = true, securedEnabled = true)
 @EnableReactiveMongoRepositories
 @EnableAutoConfiguration
-@EnableEurekaClient
 public class PostServiceApplication {
 
 	public static void main(String[] args) {
